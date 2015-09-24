0a51978798c6baa683e9047c179485d255c401b4
Randal Baker <rbaker1325@gmail.com>
2015-09-24 02:18:14 -0400

    now have one sign able to be displayed(Aries). the others just have to be copied in.

--- app/src/main/java/test/zodiacsign/android/zodiacsign/ShowSignActivity.java
+++ app/src/main/java/test/zodiacsign/android/zodiacsign/ShowSignActivity.java
@@ -9,6 +9,8 @@ import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class ShowSignActivity extends AppCompatActivity {
@@ -19,26 +21,47 @@ public class ShowSignActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_sign);
-        usersAge =(TextView)findViewById(R.id.SignTextView);
+        usersAge = (TextView) findViewById(R.id.SignTextView);
 
 
+        usersAge.setText(getSign());
 
 
+    }
+    private String getSign() {
         Date birthday = (Date) this.getIntent().getExtras().get("TheBirthDay");
 
+        if (birthday.compareTo( ariesStart()) >= 0 && birthday.compareTo(ariesEnd())<=0)
+        {
+            return "Aries";
+        }
 
-
-
-
-        //usersAge.setText("Your Age is " + String.valueOf(age));
-
-
+        return"couldn't locate your sign";
     }
 
 
-
-  
-
+    private Date ariesStart()//this is one day off so that it register on the exact day. for some reason it wouldnt.
+    {
+        Date birthday = (Date) this.getIntent().getExtras().get("TheBirthDay");
+        int birthdayYear = birthday.getYear() + 1900;
+        Calendar cal = Calendar.getInstance();
+        cal.set(birthdayYear, Calendar.MARCH, 20); //Year, month and day of month
+        Date ariesStart = cal.getTime();
+        return ariesStart;
     }
+    private Date ariesEnd()
+    {
+        Date birthday = (Date) this.getIntent().getExtras().get("TheBirthDay");
+        int birthdayYear = birthday.getYear() + 1900;
+        Calendar cal = Calendar.getInstance();
+        cal.set(birthdayYear, Calendar.APRIL, 19); //Year, month and day of month
+        Date ariesEnd = cal.getTime();
+        return ariesEnd;
+    }
+
+}
+
+
+
 
 
